8f2d2b8dd72c54d2062ae4d08bc0edbb9f80f51f
dinhxuanhongquan <quandinh.09022003@gmail.com>
2024-01-06 05:42:57 +0700

    file config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?
+    /*
+        Thông tin kết nối CSDL.
+        Người viết:
+        Ngày viết:
+
+    */
+    define('DB_HOST','localhost');
+    define('DB_NAME','dp_ct07');
+    define('DB_USER','admin_db_ct07');
+    define('DB_PASS','123dpct07');
+
+
+?>
